350447ff7a33cac5d1e4ae8a3a2721d18057a4e4
Tommy Fisher <tommybfisher@gmail.com>
2020-01-28 22:02:04 +0200

    Log client side route change

--- web/src/client.js
+++ web/src/client.js
@@ -1,5 +1,21 @@
 import * as sapper from '@sapper/app'
+import { log } from './utils/logging'
 
 sapper.start({
   target: document.querySelector('#sapper'),
 })
+
+let currentPath = window.location.href
+
+// Log route change
+// Note: listening to popstate does not work
+// need to poll
+window.setInterval(async () => {
+  if (window.location.href !== currentPath) {
+    await log.info(`Route: ${window.location.href}`, {
+      type: 'client',
+    })
+
+    currentPath = window.location.href
+  }
+}, 100)
