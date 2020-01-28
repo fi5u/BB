bb47c8206fb4038df72afcdf1681bbc21962a1d8
Tommy Fisher <tommybfisher@gmail.com>
2020-01-28 22:08:20 +0200

    Client route logging use pathname not full href

--- web/src/client.js
+++ web/src/client.js
@@ -5,17 +5,17 @@ sapper.start({
   target: document.querySelector('#sapper'),
 })
 
-let currentPath = window.location.href
+let currentPath = window.location.pathname
 
 // Log route change
 // Note: listening to popstate does not work
 // need to poll
 window.setInterval(async () => {
-  if (window.location.href !== currentPath) {
-    await log.info(`Route: ${window.location.href}`, {
+  if (window.location.pathname !== currentPath) {
+    await log.info(`Route: ${window.location.pathname}`, {
       type: 'client',
     })
 
-    currentPath = window.location.href
+    currentPath = window.location.pathname
   }
 }, 100)
